2773aeedc585a869ea72b2c9b27c8d60b92c662c
vladimir <vladimirsaakian@gmail.com>
2021-11-11 15:44:59 -0800

    completed task 3 and updated task 1

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ class Person {
   }
 
   eat(food){
-    if (this.stomach.length > 10){
+    if (this.stomach.length > 9){
       return;
     }
     this.stomach.push(food);
@@ -112,7 +112,14 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-  
+  constructor(obj){
+    this.name = obj.name;
+    this.age = obj.age;
+    this.location = obj.location;
+  }
+  speak(){
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
 }
 
 /*
@@ -129,7 +136,7 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
+class Instructor  {
 
 }
 /*
@@ -147,8 +154,8 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
-   
+class Student  {
+  
 }
 
 /*
@@ -165,7 +172,7 @@ class Student {
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
 class ProjectManager {
-   
+  
 }
 /*
   STRETCH PROBLEM (no tests!)
