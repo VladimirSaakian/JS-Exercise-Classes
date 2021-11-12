f9450964602792cd73913e6de575373ecb97e72a
vladimir <vladimirsaakian@gmail.com>
2021-11-11 16:21:40 -0800

    completed task 6

--- index.js
+++ index.js
@@ -214,8 +214,26 @@ class Student extends Lambdasian {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-  
+class ProjectManager extends Instructor {
+  constructor(obj) {
+    super({
+      name: obj.name,
+      age: obj.age,
+      location: obj.location,
+      specialty: obj.specialty,
+      favLanguage: obj.favLanguage,
+      catchPhrase: obj.catchPhrase,
+    });
+    this.gradClassName = obj.gradClassName;
+    this.favInstructor = obj.favInstructor;
+  }
+  standUp(channel) {
+    return `${this.name} announces to ${channel}, @channel standy times!`;
+  }
+
+  debugsCode(student, subject) {
+    return `${this.name} debugs ${student.name}'s code on ${subject}`;
+  }
 }
 /*
   STRETCH PROBLEM (no tests!)
